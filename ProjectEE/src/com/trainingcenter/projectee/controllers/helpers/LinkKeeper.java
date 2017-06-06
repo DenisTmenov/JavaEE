eda9848368dcc99accf27b040fd110ca73937194
DenisTmenov <denis.tmenov@gmail.com>
2017-06-06 17:47:06 +0300

    LinkKeeper WriterOutJsp

---
+++ ProjectEE/src/com/trainingcenter/projectee/controllers/helpers/LinkKeeper.java
@@ -0,0 +1,17 @@
+package com.trainingcenter.projectee.controllers.helpers;
+
+public final class LinkKeeper {
+
+	public static final String START_PAGE = "./index.html";
+	public static final String WELCOME_PAGE = "./welcome.html";
+	public static final String FORGOT_PAGE = "./forgot.html";
+	public static final String USER_PAGE = "./userpage.html";
+
+	public static final String JSP_MAIN_INTERFACE = "/WEB-INF/pages/xxxx.jsp";
+	public static final String JSP_WELCOME_LOGIN = "/WEB-INF/pages/welcomelogin.jsp";
+	public static final String JSP_REGISTRATION = "/WEB-INF/pages/registration.jsp";
+
+	public static final String TEXT_FORGOT_DEL = "/WEB-INF/txtTemplates/forgotDEL.txt";
+	public static final String TEXT_FORGOT_OK = "/WEB-INF/txtTemplates/forgotOK.txt";
+
+}
---
+++ ProjectEE/src/com/trainingcenter/projectee/controllers/helpers/WriterOutJsp.java
@@ -0,0 +1,20 @@
+package com.trainingcenter.projectee.controllers.helpers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public final class WriterOutJsp {
+
+	public static void writeEmailIsSend(String email, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<p> Login and password were sent to the address - " + email + "</p>");
+		out.println("<a class=' login-detail-panel-button btn'");
+		out.print("href='");
+		out.print(LinkKeeper.START_PAGE);
+		out.print("'> <i class='fa fa-arrow-left'></i>");
+		out.println("Go back to Homepage</a>");
+	}
+
+}
